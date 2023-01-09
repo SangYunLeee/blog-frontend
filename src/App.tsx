3e9c85dbe3024113c87e006411a78ac5fc94e6d1
sangyun lee <sororiri@gmail.com>
2023-01-09 14:44:24 +0900

    ADD: 초기 파일 셋팅

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function App() {
+  const testFunt = () => {
+    for (let i = 0; i < 10; i++) {
+      console.log(i);
+    }
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div onClick={testFunt}>테스트</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
