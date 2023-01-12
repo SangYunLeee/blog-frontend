76c46764b58617a11e3678036910508c1d5d630a
kiminSong <kiminsee22@gmail.com>
2023-01-12 18:10:18 +0900

    MODIFY: json 데이터 추가 및 css변경

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,31 +1,23 @@
 import React from 'react';
 import css from './Blog.module.scss';
+import type { BlogData } from '../../pages/BlogPage/BlogPage';
 
-const Blog = () => {
+const Blog = (
+  props: Pick<BlogData, 'title' | 'content' | 'reply' | 'img' | 'date'>
+) => {
+  const { title, content, reply, img, date } = props;
   return (
     <div className={css.blogContainner}>
-      <h1 className={css.blogName}>갚아봐요 대출의 숲</h1>
-      <div className={css.contentWrapper}>
-        <h2 className={css.allContents}>전체 글</h2>
-        <hr className={css.border} />
-      </div>
       <section className={css.contents}>
         <div className={css.contentWrapper}>
-          <p className={css.writeDate}>2023.01.01</p>
+          <p className={css.writeDate}>{date}</p>
           <div className={css.titleWrapper}>
-            <h3 className={css.contentTitle}>블로그 제목</h3>
-            <p className={css.reply}>(5)</p>
+            <h3 className={css.contentTitle}>{title}</h3>
+            <p className={css.reply}>({reply})</p>
           </div>
-          <p className={css.content}>
-            블로그 글의 내용들 블로그 글의 내용들블로그 글의 내용들블로그 글의
-            내용들블로그 글의 내용들블로그 글의 내용들블로그 글의 내용들
-          </p>
+          <p className={css.content}>{content}</p>
         </div>
-        <img
-          className={css.contentImg}
-          src="https://images.unsplash.com/photo-1673322062448-6a50cf43876a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80"
-          alt="BlogImg"
-        />
+        <img className={css.contentImg} src={img} alt="BlogImg" />
       </section>
     </div>
   );
--- src/pages/BlogPage/BlogPage.tsx
+++ src/pages/BlogPage/BlogPage.tsx
@@ -4,8 +4,16 @@ import Blog from '../../components/Blog/Blog';
 import Profile from '../../components/Blog/Profile';
 import Category from '../../components/Blog/Category';
 
+export interface BlogData {
+  id: number;
+  title: string;
+  content: string;
+  reply: number;
+  img: string;
+  date: string;
+}
 const BlogPage = () => {
-  const [blogData, setBlogData] = useState([]);
+  const [blogData, setBlogData] = useState<BlogData[]>([]);
 
   useEffect(() => {
     fetch('../data/blogdata.json')
@@ -19,7 +27,18 @@ const BlogPage = () => {
         <Profile />
         <Category />
       </div>
-      <Blog />
+      <div className={css.blogcontent}>
+        <div className={css.blogHeader}>
+          <h1 className={css.blogName}>갚아봐요 대출의 숲</h1>
+          <div className={css.titleWrapper}>
+            <h2 className={css.allContents}>전체 글</h2>
+            <hr className={css.border} />
+          </div>
+        </div>
+        {blogData.map((data) => {
+          return <Blog key={data.id} {...data} />;
+        })}
+      </div>
     </div>
   );
 };
