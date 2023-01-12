b359ba8584dc230cd259091a54c3f8c17c126059
kiminSong <kiminsee22@gmail.com>
2023-01-12 23:54:02 +0900

    ADD: SettingPage 레이아웃

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BlogPage from './BlogPage/BlogPage';
+import SettingPage from './SettingPage/SettingPage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/blog" element={<BlogPage />} />
+        <Route path="/setting" element={<SettingPage />} />
       </Routes>
     </BrowserRouter>
   );
