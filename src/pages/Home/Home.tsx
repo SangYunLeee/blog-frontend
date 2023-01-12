621ba2d30a3d00a7ad75d0984c54a215bac38581
SulhwaChoi <imsulhwa00@gmail.com>
2023-01-12 18:38:55 +0900

    ADD: 팔로우 레이아웃 완료

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,19 @@
+import React, { useState, Dispatch, SetStateAction } from 'react';
+import Follow from '../../components/Follow/Follow';
+
+export interface FollowBtnType {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const Home = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  return (
+    <div>
+      <button onClick={() => setOpen(true)}>팔로우</button>
+      {open ? <Follow open={open} setOpen={setOpen} /> : null}
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import Home from './Home/Home';
+
 function Router() {
   return (
     <BrowserRouter>
