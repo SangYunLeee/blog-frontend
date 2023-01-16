d01c5b572e242ddfb633a7b0247e68ad87212576
SulhwaChoi <imsulhwa00@gmail.com>
2023-01-16 13:57:03 +0900

    ADD: home.tsx파일에 follow 코드 주석처리

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,18 @@
 import React, { useState, Dispatch, SetStateAction } from 'react';
-import Follow from '../../components/Follow/Follow';
+// import Follow from '../../components/Follow/Follow';
 
-export interface FollowBtnType {
-  open: boolean;
-  setOpen: Dispatch<SetStateAction<boolean>>;
-}
+// 팔로우 연결
+// export interface FollowBtnType {
+//   open: boolean;
+//   setOpen: Dispatch<SetStateAction<boolean>>;
+// }
 
 const Home = () => {
-  const [open, setOpen] = useState<boolean>(false);
+  // const [open, setOpen] = useState<boolean>(false);
   return (
     <div>
-      <button onClick={() => setOpen(true)}>팔로우</button>
-      {open ? <Follow open={open} setOpen={setOpen} /> : null}
+      {/* <button onClick={() => setOpen(true)}>팔로우</button> */}
+      {/* {open ? <Follow open={open} setOpen={setOpen} /> : null} */}
     </div>
   );
 };
