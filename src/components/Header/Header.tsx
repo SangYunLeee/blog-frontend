3caa2bffe5e5ae8c9f4ecf083a0af9fba61c2aa7
SulhwaChoi <imsulhwa00@gmail.com>
2023-02-06 11:47:13 +0900

    EDIT : css 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -76,7 +76,10 @@ const Header = () => {
           className={css.logo}
           onClick={() => (window.location.href = 'http://localhost:3000')}
         >
-          <img src="./image/logo.png" alt="" />
+          <img
+            src={process.env.REACT_APP_PUBLIC_URL + '/image/logo.png'}
+            alt=""
+          />
         </div>
         <div className={css.headerRightArea}>
           <div className={css.searchbar}>
